0c9df8df5e985a5b20976b2b3e021ca1aefb383c
unknown <swati.mani@mavs.uta.edu>
2019-06-14 01:25:56 -0500

    Modified

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # from flask import Flask
-from flask import *
+from flask import Flask, render_template, request
+import sqlite3 as sql
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "hello flask"
+def index():
+    return render_template('index.html')
+
+@app.route('/enternew')
+def upload_csv():
+    return render_template('upload.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+       con = sql.connect("database.db")
+       csv = request.files['myfile']
+       file = pd.read_csv(csv)
+       file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)
+       con.close()
+       return render_template("result.html",msg = "Record inserted successfully")
 
 if __name__ == '__main__':
   app.run()
