8446c1f2345d598f8d6657bcfc1c317f4d4c7f26
unknown <swati.mani@mavs.uta.edu>
2019-06-18 19:34:32 -0500

    pushed2

--- main.py
+++ main.py
@@ -182,21 +182,33 @@ def addrec():
 #Display according to starting letter and select id accordingly
 @app.route('/append_To_string',methods=['GET','POST'])
 def append_To_string():
-    query='select id from Earthquake where id LIKE "O%"'
-    con = sql.connect("database.db")
-    cur = con.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    s_time=time.time()
-    val=random.randint(0,len(rows)-1)
-    var=str(rows[val])
-    query1="select * from earthquake where id ='"+var[2:12]+"'"
-    con = sql.connect("database.db")
-    cur = con.cursor()
-    cur.execute(query1)
-    rows1 = cur.fetchall()
-    e_time=time.time()-s_time
-    return render_template("table_display.html",data=rows, stime=e_time)
+    if request.method=='POST':
+        d1=float(request.form['d1'])
+        d2=float(request.form['d2'])
+        loop=int(request.form['loop'])
+        s_time = time.time()
+        for i in range(loop):
+            query = "select depthError from Earthquake where depthError >=" + str(d1) + " and depthError<=" + str(d2) + ""
+            con = sql.connect("database.db")
+            cur = con.cursor()
+            cur.execute(query)
+            rows = cur.fetchall()
+
+            var=[]
+            var1=[]
+            val=random.randint(0,len(rows)-1)
+            val1 = random.randint(0, len(rows) - 1)
+            var.append(rows[val])
+            var1.append(rows[val1])
+            print(var)
+            print(var1)
+            query1="select depthError from Earthquake where depthError >='"+str(var[3:6])+"' and depthError <='"+str(var1[3:5])+"'"
+            con = sql.connect("database.db")
+            cur = con.cursor()
+            cur.execute(query1)
+            rows1 = cur.fetchall()
+        e_time=time.time()-s_time
+        return render_template("table_display.html",data=rows1, stime=e_time)
 
 #Display according to starting letter and select id accordingly with cache
 @app.route('/append_cache',methods=['GET','POST'])
